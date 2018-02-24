bf41cfd05d028fd33a88c35e376fdbc5bdeabf12
银翘 <yinqiao.zrj@alibaba-inc.com>
2018-02-24 19:52:59 +0800

    add utils

--- app/Game.js
+++ app/Game.js
@@ -2,25 +2,7 @@ import React from 'react';
 import Board from 'Board.js';
 import Clock from 'Clock.js';
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+import { calculateWinner } from 'Utils.js'
 
 class Game extends React.Component {
   constructor() {
